1487dc61bc62c27be28b5629e98dc1583e4ca837
Jayad Arshad <jayadusama@hotmail.co.uk>
2022-09-04 05:00:53 +0100

    added all questions and answers

--- questions.js
+++ questions.js
@@ -30,4 +30,77 @@ const questions= [
               "4. parentheses",
          ],
         answer: "3. quotes",
-    }
+    },
+    {
+        questionText: "Inside which HTML element do we put the Javascript?",
+        options: [
+            "1.script",
+            "2.javascript",
+            "3.scripting",
+            "4.js",
+
+         ],
+        answer: "1.script",
+    },
+    {
+        questionText: "Arrays in JavaScript can be used to store ______.",
+        options: [
+            "1. numbers and strings",
+            "2. other arrays",
+            "3. booleans",
+            "4. all of the above",
+         ],
+        answer: "4. all of the above",
+    },
+    {
+        questionText: "Commonly used data types DO NOT include:",
+        options: [
+            "1. strings",
+            "2. booleans", 
+            "3. alerts", 
+            "4. numbers",
+         ],
+        answer: "3. alerts",
+    },
+    {
+        questionText: "",
+        options: [
+            "1.break",
+            "2. stop",
+            "3. halt",
+            "4.exit",
+         ],
+        answer: "1.break",
+    },
+    {
+        questionText: "What is the correct syntax for writing an 'IF' statement?",
+        options: [
+            "1.if i == 5 then",
+            "2.if i == 5",
+            "3.if i = 5 then",
+            "4.if (i == 5)",
+         ],
+        answer: "4.if (i == 5)",
+    },
+    {
+        questionText: "Which operator is used to assign a value to a variable",
+        options: [
+            "1. *",
+            "2. -",
+            "3. +",
+            "4. =",
+         ],
+        answer: "4. =",
+    },
+    {
+        questionText: "Where is the best practice for placing your JS script?",
+        options: [
+            "1.At the beginning of the head",
+            "2.At the beginning of the body",
+            "3.At the end of the body",
+            "4.At the end of the head",
+         ],
+        answer: "3.At the end of the body",
+    },
+
+]

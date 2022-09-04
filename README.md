383a63f685c9f2d2f18bd89a41d5f871a6461f98
Jayad Arshad <jayadusama@hotmail.co.uk>
2022-09-04 05:16:12 +0100

    complete pseudo and readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # code-quiz-2
+
+# Description 
+I have created a fun and simple quiz that allows users to test their coding knowledge. When each questions is presented they have a time limit to answer the questions in order to move on. However if the user selects the wrong answer, 10 seconds are deducted from the timer. 
+
+
+# Functionality:
+A simple, self explanatory quiz which allows the user to click start, triggering the timer. The first question is displayed on the screen to which the user has to select the correct answer. Failure to do so, will result a deduction of 10 seconds on the timer. The message "incorrect answer" will also be displayed at the same time. In order to move to the next question, the user needs to select the correct answer. Every wrong answer results in a 10second deduction on the timer, once it reaches 0 the game comes to an end.
+
+The user is then taken to another page where they find their final score and they will be asked for their initals. When the user clicks submit they will have submitted their highscore to a score board, the user then has the option to clear this highscore and go back to start the quiz again.
+
+# Technologies Used
+* HTML
+* CSS
+* Javascript
+
+
+# Deployed URL 
+https://jayad619.github.io/code-quiz-2/
+
+# Github URL 
+https://github.com/Jayad619/code-quiz-2
+
+# Screens
+<img width="1512" alt="Screenshot 2022-09-04 at 05 13 36" src="https://user-images.githubusercontent.com/102623563/188297081-78f5b395-dac3-470c-8e68-3146bc63b36b.png">
--- pseudo.js
+++ pseudo.js
@@ -23,3 +23,69 @@ const onLoad = () => {
 
     // setInterval of 1000ms (1s)
   };
+  const validateAnswer = () => {
+    // get answer clicked from user
+    // get the correct answer for question
+    // compare the 2 answers
+    // if incorrect subtract 5 seconds from timerValue
+    // if incorrect render error alert with message and status
+    // if correct render success alert with message and status
+    // set timeout for 500ms and then go to next question
+    // if question is last question set quizComplete to true and then render form
+    // if question is not last question then increment question index and render next question
+  };
+
+  const handleFormSubmit = () => {
+    // get value from input
+    // check if empty then render error alert with message and status
+    // if not empty then create the score object
+    // {
+    //   fullName: "Bob Smith",
+    //   score: 25
+    // }
+    // push score object to LS
+    // render quizCompleteSection
+  };
+
+  const renderTimerSection = () => {
+    // use HTML as guide and build in JS
+    // append section to main
+  };
+
+  const renderQuestionSection = () => {
+    // use HTML as guide and build in JS
+    // append section to main
+    // add click event listener on #question-section
+  };
+
+  const renderGameOver = () => {
+    // use HTML as guide and build in JS
+    // append section to main
+  };
+
+  const renderAlert = (message, status) => {
+    // use HTML as guide and build in JS
+    // append div to #question-section
+  };
+
+  const renderForm = () => {
+    // use HTML as guide and build in JS
+    // append section to main
+    // add submit event handler to form
+  };
+
+  const renderQuizCompleteSection = () => {
+    // use HTML as guide and build in JS
+    // append section to main
+  };
+
+  const startQuiz = () => {
+    // remove start section
+    // start timer
+    // render timer section
+    // render question section
+  };
+
+  // add event listeners
+  // add document on load event listener
+  // add start button click event listener

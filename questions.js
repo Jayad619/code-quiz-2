1fd3df544e9dd4c64a20b9688bf10971781422b8
Jayad Arshad <jayadusama@hotmail.co.uk>
2022-09-04 04:56:10 +0100

    added 3 questions

---
+++ questions.js
@@ -0,0 +1,33 @@
+// Questions and Answers
+
+const questions= [
+    {
+        questionText: "Which of the following statements can be used to terminate a loop,switch or label statement",
+        options: [
+            "1.break",
+            "2. stop",
+            "3. halt",
+            "4.exit",
+         ],
+        answer: "1.break",
+    },
+    {
+        questionText: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        options: [
+            "1. JavaScript",
+            "2. terminal/bash",
+            "3. for loops",
+            "4. console.log",
+         ],
+        answer: "4. console.log",
+    },
+    {
+        questionText: "String values must be enclosed within _____ when being assigned to variables.",
+        options: [
+             "1. commas", 
+             "2. curly brackets", 
+             "3. quotes",
+              "4. parentheses",
+         ],
+        answer: "3. quotes",
+    }

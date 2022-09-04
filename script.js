6e5e0bc84276bd2372712ee92f2bc43b143a3352
Jayad Arshad <jayadusama@hotmail.co.uk>
2022-09-04 04:35:48 +0100

    details

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ const startBtn = document.getElementById("js-quiz-start");
   window.addEventListener("load", () => {
       game = new Game();
   });
+
+  document.querySelector("#js-view-leaderboard").addEventListener("click", () => {
+    if (!game.timer) {
+        document.querySelector("#js-section-intro").classList.remove("active");
+        HighScores.viewHighScores();
+    }
+});
+
+document.querySelector(".highscore-actions__back").addEventListener("click", () => {
+    document.querySelector("#js-section-leaderboard").classList.remove("active");
+    document.querySelector("#js-section-intro").classList.add("active");
+});
+
+
+document.querySelector(".highscore-actions__clear").addEventListener("click", () => {
+    if (confirm("Do you really want to remove all your highscores?")) {
+        HighScores.clear();
+        HighScores.viewHighScores();
+    }
+});

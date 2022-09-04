9a953238372815be9102bb45ee0f3f414a94b6c4
Jayad Arshad <jayadusama@hotmail.co.uk>
2022-09-04 04:34:28 +0100

    start game section

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 const startBtn = document.getElementById("js-quiz-start");
+
+  // start state of game
+  let game = null;
+
+
+  /**
+   * Generic event listeners
+   */
+  window.addEventListener("load", () => {
+      game = new Game();
+  });

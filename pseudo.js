729d9347587ddba88b38af0db208e1176c029bc3
Jayad Arshad <jayadusama@hotmail.co.uk>
2022-09-04 05:06:19 +0100

    functions

--- pseudo.js
+++ pseudo.js
@@ -3,3 +3,23 @@ const questions = [];
 let questionIndex = 0;
 let timerValue = 10 * questions.length;
 let quizComplete = false;
+
+const onLoad = () => {
+    // initialise local storage
+    // check if highscores exists in LS
+    // if false then set highscores to empty array in LS
+  };
+
+  const removeStartSection = () => {};
+
+  const startTimer = () => {
+    // declare function to execute every 1 sec
+    const countdown = () => {
+      // decrement timer value
+      // if quizComplete is true then stop timer
+      // check if timer reaches 0
+      // if true render game over
+    };
+
+    // setInterval of 1000ms (1s)
+  };
